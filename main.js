0bb058f33f322a4822cdc57a72abca398f37ff6f
Zachary Williams <wil11063@byui.edu>
2018-05-22 11:45:20 -0600

    Add campus options

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ module.exports = (course, stepCallback) => {
     }
 
     function updateCourse(callback) {
-        var testObj = {
+
+        var onlineCourse = {
             'course[license]': 'private',
             'course[is_public_to_auth_users]': false,
             'course[is_public]': false,
@@ -24,8 +25,24 @@ module.exports = (course, stepCallback) => {
             'course[time_zone]': 'America/Denver',
             'course[grading_standard_id]': 1,
             'course[sis_course_id]': buildSISID()
-        };
-        canvas.put(`/api/v1/courses/${course.info.canvasOU}`, testObj, (err, newCourse) => {
+        }
+
+        var campusCourse = {
+            'course[license]': 'private',
+            'course[is_public_to_auth_users]': false,
+            'course[is_public]': false,
+            'course[public_syllabus_to_auth]': true,
+            'course[course_format]': 'online',
+            'course[term_id]': 5,
+            'course[locale]': 'en',
+            'course[time_zone]': 'America/Denver',
+            'course[grading_standard_id]': 1,
+            'course[sis_course_id]': `${buildSISID()}-InstructorLastName`
+        }
+
+        var putObj = course.settings.platform === 'online' ? onlineCourse : campusCourse;
+
+        canvas.put(`/api/v1/courses/${course.info.canvasOU}`, putObj, (err, newCourse) => {
             if (err) {
                 course.error(err);
                 callback(null);
@@ -37,13 +54,26 @@ module.exports = (course, stepCallback) => {
     }
 
     function updateSettings(callback) {
-        var putObj = {
+
+        var campusObj = {
             'lock_all_announcements': false,
             'allow_student_forum_attachments': true,
             'show_announcements_on_home_page': true,
             'allow_student_organized_groups': false,
             'home_page_announcement_limit': 2,
+        }
+
+        var onlineObj = {
+            'lock_all_announcements': false,
+            'allow_student_forum_attachments': true,
+            'show_announcements_on_home_page': true,
+            'allow_student_discussion_editing': true,
+            'allow_student_organized_groups': true,
+            'home_page_announcement_limit': 2,
         };
+
+        var putObj = course.settings.platform === 'online' ? campusObj : onlineObj;
+
         canvas.put(`/api/v1/courses/${course.info.canvasOU}/settings`, putObj, (err, newSettings) => {
             if (err) {
                 course.error(err);
@@ -73,13 +103,6 @@ module.exports = (course, stepCallback) => {
     /**************
      * START HERE
      *************/
-
-    var validPlatforms = ['online', 'pathway'];
-    if (!validPlatforms.includes(course.settings.platform)) {
-        course.message('Invalid platform. Skipping child module');
-        stepCallback(null, course);
-        return;
-    }
 
     var tasks = [
         updateCourse,
