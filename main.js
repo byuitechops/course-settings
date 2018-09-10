c3767299cd893c4cccb14e6ec9fb67c8bf979d3e
Danverde <gre12014@byui.edu>
2018-09-10 13:33:36 -0600

    Now adds enrollment term to campus courses

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 /*eslint-env node, es6*/
 
-/* Module Description */
+/*****************************************************
+ * This child module sets course settings according
+ * to predefined standards and user input for online,
+ * campus, and pathway courses
+ ****************************************************/
 
 const canvas = require('canvas-wrapper');
 const asyncLib = require('async');
 
 module.exports = (course, stepCallback) => {
+    function getTermID(termName, cb) {
+        if (!termName) {
+            cb(null, null);
+        }
+        /* use top account for enrollment terms */
+        canvas.get('/api/v1/accounts/1/terms', (err, terms) => {
+            if (err) {
+                cb(err, null);
+                return;
+            }
+
+            try {
+                terms = terms[0].enrollment_terms;
+
+                var term = terms.find(term => term.name === termName);
+                if (term == undefined) {
+                    throw new Error('Unable to find matching term');
+                } else {
+                    cb(null, term.id);
+                }
+
+            } catch (findErr) {
+                cb(findErr, null);
+            }
+        });
+    }
 
     function buildSISID() {
         var platform = course.settings.platform[0].toUpperCase() + course.settings.platform.slice(1);
@@ -13,42 +43,59 @@ module.exports = (course, stepCallback) => {
     }
 
     function updateCourse(callback) {
-        var onlineCourse = {
-            'course[license]': 'private',
-            'course[is_public_to_auth_users]': false,
-            'course[is_public]': false,
-            'course[public_syllabus_to_auth]': true,
-            'course[course_format]': 'online',
-            'course[term_id]': 5,
-            'course[locale]': 'en',
-            'course[time_zone]': 'America/Denver',
-            'course[grading_standard_id]': 1,
-            'course[sis_course_id]': buildSISID()
-        };
+        function buildPutObj(cb) {
+            var onlineCourse = {
+                'course[license]': 'private',
+                'course[is_public_to_auth_users]': false,
+                'course[is_public]': false,
+                'course[public_syllabus_to_auth]': true,
+                'course[course_format]': 'online',
+                'course[term_id]': 5,
+                'course[locale]': 'en',
+                'course[time_zone]': 'America/Denver',
+                'course[grading_standard_id]': 1,
+                'course[sis_course_id]': buildSISID()
+            };
+            var campusCourse = {
+                'course[license]': 'private',
+                'course[is_public_to_auth_users]': false,
+                'course[is_public]': false,
+                'course[public_syllabus_to_auth]': true,
+                'course[course_format]': 'on_campus',
+                'course[term_id]': 5,
+                'course[locale]': 'en',
+                'course[time_zone]': 'America/Denver',
+                'course[grading_standard_id]': 1,
+                'course[sis_course_id]': `${buildSISID()}-InstructorLastName`
+            };
 
-        var campusCourse = {
-            'course[license]': 'private',
-            'course[is_public_to_auth_users]': false,
-            'course[is_public]': false,
-            'course[public_syllabus_to_auth]': true,
-            'course[course_format]': 'on_campus',
-            'course[term_id]': 5,
-            'course[locale]': 'en',
-            'course[time_zone]': 'America/Denver',
-            'course[grading_standard_id]': 1,
-            'course[sis_course_id]': `${buildSISID()}-InstructorLastName`
-        };
+            if (course.settings.platform !== 'campus') {
+                cb(onlineCourse);
+            } else {
+                getTermID(course.settings.term, (err, termId) => {
+                    if (err) {
+                        course.error(err);
+                    } else if (termId === null) {
+                        course.error(new Error('Unable to determine termID'));
+                    } else {
+                        campusCourse['course[term_id]'] = termId;
+                    }
 
-        var putObj = course.settings.platform === 'online' || course.settings.platform === 'pathway' ? onlineCourse : campusCourse;
-
-        canvas.put(`/api/v1/courses/${course.info.canvasOU}`, putObj, (err, newCourse) => {
-            if (err) {
-                course.error(err);
-                callback(null);
-                return;
+                    cb(campusCourse);
+                });
             }
-            course.message('Course updated successfully.');
-            callback(null);
+        }
+
+        buildPutObj((putObj) => {
+            canvas.put(`/api/v1/courses/${course.info.canvasOU}`, putObj, (err, newCourse) => {
+                if (err) {
+                    course.error(err);
+                    callback(null);
+                    return;
+                }
+                course.message('Course updated successfully.');
+                callback(null);
+            });
         });
     }
 
